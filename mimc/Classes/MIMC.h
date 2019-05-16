9c8253f64e05bfaf04d77a227dd78d01bcdb966f
v39lfy <admin@onlywish.me>
2019-05-16 15:40:54 +0800

    新标签

---
+++ mimc/Classes/MIMC.h
@@ -0,0 +1,16 @@
+//
+//  MIMC.h
+//  Expecta
+//
+//  Created by 福有李 on 2019/5/16.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MIMC : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ mimc/Classes/MIMC.m
@@ -0,0 +1,12 @@
+//
+//  MIMC.m
+//  Expecta
+//
+//  Created by 福有李 on 2019/5/16.
+//
+
+#import "MIMC.h"
+
+@implementation MIMC
+
+@end
